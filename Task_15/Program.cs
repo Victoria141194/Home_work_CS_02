4f982d5249e031b8595220fe46d8cc5b6dffe2e1
Victoria <victoria.kainova@gmail.com>
2022-10-06 23:39:05 +0300

    Added task 15

---
+++ Task_15/Program.cs
@@ -0,0 +1,22 @@
+﻿int userInput;
+
+while (true)
+{
+    Console.Write("Введите цифру дня недели: ");
+    if (int.TryParse(Console.ReadLine(), out userInput))
+        break;
+    Console.WriteLine($"Ошибка ввода!");
+}
+
+if (userInput <= 5)
+{
+    Console.WriteLine($"{userInput} -> будний день");
+}
+
+if (6 <= userInput && userInput <= 7)
+{
+    Console.WriteLine($"{userInput} -> выходной день");
+}
+
+if (userInput > 7)
+    Console.WriteLine($"Ошибка ввода!");
