c2e733654a6a8f66b7752bba26a819c5565e5e99
Victoria <victoria.kainova@gmail.com>
2022-10-06 23:15:10 +0300

    Added task 13

---
+++ Task_10/Program.cs
@@ -0,0 +1,14 @@
+﻿int userInput;
+
+while (true)
+{
+    Console.Write("Введите трехзначное число: ");
+    if (int.TryParse(Console.ReadLine(), out userInput))
+        if (99 < userInput && userInput < 1000)
+            break;
+    Console.WriteLine($"Ошибка ввода!");
+}
+
+int num = userInput % 100;
+int num1 = num / 10;
+Console.Write($"{userInput} -> {num}");
---
+++ Task_13/Program.cs
@@ -0,0 +1,22 @@
+﻿int userInput;
+
+while (true)
+{
+    Console.Write("Введите число: ");
+    if (int.TryParse(Console.ReadLine(), out userInput))
+        break;
+    Console.WriteLine($"Ошибка ввода!");
+}
+
+if (userInput < 100)
+{
+    Console.WriteLine($"{userInput} -> третьей цифры нет");
+}
+
+if (userInput > 100)
+{
+    string S = Convert.ToString(userInput);
+    char a = S[2];
+
+    Console.WriteLine($"третья цифра -> {a}");
+}
